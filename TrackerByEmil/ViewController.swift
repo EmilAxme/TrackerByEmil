1885db4a4af557ec474946381758288cd76332d9
EmilAxme <emil.azmedov@mail.ru>
2025-06-01 13:37:31 +0300

    Initial Commit

---
+++ TrackerByEmil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackerByEmil
+//
+//  Created by Emil on 01.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TrackerByEmilTests/TrackerByEmilTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerByEmilTests.swift
+//  TrackerByEmilTests
+//
+//  Created by Emil on 01.06.2025.
+//
+
+import Testing
+@testable import TrackerByEmil
+
+struct TrackerByEmilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
